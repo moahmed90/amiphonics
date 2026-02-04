98aa0a764f84234e9464cb0a54f052d0bca04c39
Mo Ahmed <moahmed@Mos-MacBook-Air.local>
2026-02-04 22:04:59 +0000

    V1 making app externally reachable for EC2 deployment

--- app.py
+++ app.py
@@ -30,6 +30,10 @@ def api_word():
     word = get_random_word(level)
     return jsonify({"word": word, "level": level})
 
+@app.route("/health")
+def health():
+    return jsonify({"ok":True})
+
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host ="0.0.0.0", port=5000,debug=True)
